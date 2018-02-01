1862aa640ecd8bbf48065b4d4b7736ab8a734060
Ilya Tretyakov #it3xl <it3xl@users.noreply.github.com>
2018-02-01 21:17:06 +0300

    Allows fast initial filling for the local repo. Otherwise the repo will stay empty until a first remote modification will occur.

--- change_detector.sh
+++ change_detector.sh
@@ -5,10 +5,15 @@ remote_refs_2=$(git ls-remote --heads "$url_2" $prefix_1* $prefix_2*)
 local_refs_1_sha=$(git for-each-ref --format="%(objectname)" "refs/remotes/$origin_1/")
 local_refs_2_sha=$(git for-each-ref --format="%(objectname)" "refs/remotes/$origin_2/")
 
+# Let's check the local repository is new and empty. For a fast filling of the local repo.
+remote_count=$(echo "$remote_refs_1" | awk 'END { print NR }';)
+local_count=$(echo "$local_refs_1_sha" | awk 'END { print NR }';)
+
 changes_detected=0
 if [[ "$remote_refs_1" != "$remote_refs_2" \
-      || "$local_refs_1_sha" != "$local_refs_2_sha" ]]; then
-  echo figase
+      || "$local_refs_1_sha" != "$local_refs_2_sha" \
+      || $remote_count != $local_count ]];
+then
   changes_detected=1
 fi
 
