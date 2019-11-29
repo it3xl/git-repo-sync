0d7de3971b23465cf92de3267344d6cb5fbfc965
Ilya Tretyakov #it3xl <it3xl@users.noreply.github.com>
2019-11-30 01:03:56 +0300

    Removes dynamic script injections vulnerability.

--- request-git-sync.sh
+++ request-git-sync.sh
@@ -6,7 +6,8 @@ echo Start `basename $0`
 invoke_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$invoke_path/util/set_env.sh" "$@"
 
-rm -f "$env_modifications_signal_file"
+rm -f "$env_modifications_signal_file_1"
+rm -f "$env_modifications_signal_file_2"
 
 echo
 source "$path_git_sync_util/repo_create.sh"
@@ -18,14 +19,13 @@ source "$path_git_sync_util/change_detector.sh"
 
 echo
 if (( $changes_detected == 1 )); then
-  install -D /dev/null "$env_modifications_signal_file"
+  install -D /dev/null "$env_modifications_signal_file_1"
+  install -D /dev/null "$env_modifications_signal_file_2"
   
   # Passing of remote refs to prevent excessive network requesting.
-  echo > "$env_modifications_signal_file"
-  echo "remote_refs_1=\"$remote_refs_1\"" >> "$env_modifications_signal_file"
-  echo >> "$env_modifications_signal_file"
-  echo "remote_refs_2=\"$remote_refs_2\"" >> "$env_modifications_signal_file"
-  echo >> "$env_modifications_signal_file"
+  echo "$remote_refs_1" >> "$env_modifications_signal_file_1"
+
+  echo "$remote_refs_2" >> "$env_modifications_signal_file_2"
   
   echo '@' RESULT: Synchronization requested.
 else
