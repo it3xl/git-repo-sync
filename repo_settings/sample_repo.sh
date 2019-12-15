1b699f2c94c964c9e95f5f643e3ec8b5a24928e7
Ilya Tretyakov #it3xl <it3xl@users.noreply.github.com>
2019-12-15 04:03:07 +0300

    Improving of the sample configuration file sample_repo.sh.

--- repo_settings/sample_repo.sh
+++ repo_settings/sample_repo.sh
@@ -1,75 +1,79 @@
 
-# This file will teach you how to setup a synchronization between your two Git-repositories.
-# Each such file describes a pair of remote repositories that have to be synchronized.
+# This is a configuration file for a sample synchronization project.
+# You have to create a copy of this file.
 
-
-# I believe you'll want to store a file like this somewhere else but not in my Git-repo (https://github.com/it3xl/git-sync).
-# So, you can use a sibling folder "git-sync.repo_settings" next to the git-sync root folder.
+# You have two options where to store your new configuration file.
+# You can put it next to this file (sample_repo.sh).
+# Or you can create a sibling folder named "git-sync.repo_settings" next to the git-sync root folder.
 # I.e.
-# git-sync/repo_settings/  - I expect you are here now and are reading this text in this file.
-# git-sync.repo_settings/  - put you settings file here.
+# git-sync/repo_settings/  - This is the first option.
+# git-sync.repo_settings/  - This is the second option.
+
+# Change parameters below to describe your Git remote repositories that you want to synchronize.
+
+# I consider that you've created clones from your remote Git repositories before you start to play with gitSync.
 
 
-
-# It is extraordinarily dangerous for your repos to do a mistake here.
-# More often a mess with settings from different repos will make you a pain.
-# Consider to create local repositories from your remoter repositories first.
-
-# The following settings have to be set.
-
-
-# An arbitrary folder-name to stored a sync stuff.
+# It is an arbitrary folder-name to stored your sync project.
+#
 project_folder=sample_repo
 
 
 
-# Assign here a conventional prefix for refs in your first repo. Only prefixed refs will be synced.
-# To make things simpler, I demand from you to put some char at the end here.
-# I prefer to use "/", "-" or "_". And I didn't test other.
+# Assign here a prefix for refs in your first repo.
+#
 prefix_1=a/
-# Then synced branches will be looking like this "client_co/feature_x"
 
-# Assign here a real URL to your first repo.
+# Assign real URL to your first repo.
+#
 url_1=https://your-repo1-url.org/git/my_repo.git
 
 
 
-# Assign here a conventional prefix for refs of your second repo. Only prefixed refs will be synced.
+# Assign here a prefix for refs of your second repo.
+#
 prefix_2=b-
-# Then synced branches will be looking like this "vendor_co-abc_feature"
 
-# Assign here a real URL to your second repo.
+# Assign real URL to your second repo.
 url_2=https://git.your-repo2-url.org/my_repo.git
 
 
 
 # Assign a prefix for branches with conflict solving strategy "The youngest wins".
+#
 prefix_victims=@
 
 
 
-# You have to provide some branch-name here if you want to allow deletion of conventional (synced) refs.
+# To allow deletion of branches, provide here some branch-name.
 # You can create the real branch later or change it here at any time.
+#
 must_exist_branch=${prefix_1}production
+
+
+
+
+
 
 
 ## Integration with git-cred.sh "bash Git Credential Helper" (https://github.com/it3xl/git-sync)
 # 
 # git-cred allows you to use credentials from environment variables
-#  that are defined automatically by Jenkins or any other Continues Integration (CI) tool.
+#  that are defined automatically by any Continues Integration (CI) tool.
 #
 # You can use git-cred as an external tool and tune everything manually.
-# But this integration allows you to call initializing of git-cred only once during your git-sync project creation.
-# Also it will support you during migrations and relocations of you CI.
+# But configuring it here allows you to initialize git-cred only once during your git-sync project creation.
+# Also it supports seamlessly migrations and relocations of you CI.
 #
 ## Integration steps
 #
-# Load Git submodules of git-sync (https://github.com/it3xl/git-sync)
+# Load Git sub-modules of git-sync (https://github.com/it3xl/git-sync)
 #
-# Assign here (i.e. in your settings file) the following variable to 1.
+# Assign here the following variable to 1.
+#
 # use_bash_git_credential_helper=1
 #
-# Before any call to git-sync.sh or request-git-sync.sh, define in Jenkins the following credential environment variables
+# Before any call to git-sync.sh or request-git-sync.sh, define in a CI-server (tool) the following environment variables
 #   For the repo in $url_1
 # git_cred_username_repo_1=some-login
 # git_cred_password_repo_1=some-password
